e64f565bd8594dcfa1dbf275bd4915bb1f254965
Pavel Topinka <ptopinka@gmail.com>
2011-12-21 15:40:13 +0100

    pridana galerie satu pro druzicky

--- druzicky.php
+++ druzicky.php
@@ -55,6 +55,68 @@
 				<img src="img/masle.png"/>
 			</div>
 	  
+            <!-- 1 RADA -->
+            <div>
+      				<a id="thumb1" href="img/druzicky/20110617_01.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110617_01_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="202" width="135" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+      				<a id="thumb1" href="img/druzicky/20110617_02.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110617_02_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="202" width="135" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+      				<a id="thumb1" href="img/druzicky/20110623_02.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110623_02_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="202" width="135" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+      				<a id="thumb1" href="img/druzicky/20110623_04.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110623_04_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="202" width="135" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+
+
+
+
+            </div>
+            <br/>
+
+
+            <!-- 2 RADA -->
+            <div align="center">
+      				<a id="thumb1" href="img/druzicky/20110623_09.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110623_09_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="202" width="135" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+      				<a id="thumb1" href="img/druzicky/20110623_25.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110623_25_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="135" width="202" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+      				<a id="thumb1" href="img/druzicky/20110623_27.jpg" class="highslide" onclick="return hs.expand(this)">
+      					<img src="img/druzicky/20110623_27_thumb.jpg" alt="Highslide JS"
+      						title="Click to enlarge" height="202" width="135" /></a>
+      				<div class="highslide-caption">
+      				   Šaty pro družicky
+      				</div>
+      			
+
+
+
+
+            </div>
+            <br/>
+
 		</div> <!-- end obsah-->
 			  <?php
 				printRightBanner();
